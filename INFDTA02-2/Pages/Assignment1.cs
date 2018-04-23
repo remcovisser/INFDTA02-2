7966cf48d01cbc76703feef85c7adeb681fd20b2
Remco Visser <remco.visser@maximum.nl>
2018-04-23 15:25:41 +0200

    Stuff

--- INFDTA02-2/Pages/Assignment1.cs
+++ INFDTA02-2/Pages/Assignment1.cs
@@ -9,9 +9,14 @@ namespace INFDTA02_2.Pages
 {
     public class IndexModel : PageModel
     {
+        public int[] data { get; set; }
+        public string test { get; set; }
+
         public void OnGet()
         {
-
+            data = new int[] { 12, 19, 3, 5, 2, 3 };
+            test = "This is a test";
         }
+
     }
 }
--- INFDTA02-2/Pages/Index.cshtml.cs
+++ INFDTA02-2/Pages/Index.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace INFDTA02_2.Pages
 {
-    public class IndexModel : PageModel
+    public class Assignment1Model : PageModel
     {
         public void OnGet()
         {
